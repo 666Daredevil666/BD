ab466e960fa820c23dd6ab2b5fac923d64d260ff
Alexey <142442789+666Daredevil666@users.noreply.github.com>
2024-12-10 11:25:47 +0300

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,1 +1,17 @@
+--функция
+CREATE OR REPLACE FUNCTION log_egg_state_change()
+RETURNS TRIGGER AS $$
+BEGIN
+IF NEW.state <> OLD.state THEN
+INSERT INTO egg_log (egg_id, old_state, new_state)
+        		VALUES (OLD.id, OLD.state, NEW.state);
+    	END IF;
+    	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+--тригер
+CREATE TRIGGER trigger_egg_state_change
+AFTER UPDATE ON egg
+FOR EACH ROW
+EXECUTE FUNCTION log_egg_state_change();
 
